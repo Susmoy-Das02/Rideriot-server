023a79a532952fab3b3e5472f3778548b0c11810
Susmoy-Das02 <methundas269@gmail.com>
2022-05-09 01:46:03 +0600

    restock and delivered done

--- index.js
+++ index.js
@@ -12,40 +12,54 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abv9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const itemcollection = client.db('rideriot').collection('item');
-        
-        app.get('/item', async(req, res) =>{
+
+        app.get('/item', async (req, res) => {
             const query = {};
-        const cursor = itemcollection.find(query);
-        const items = await cursor.limit(6).toArray();
-        res.send(items);
+            const cursor = itemcollection.find(query);
+            const items = await cursor.limit(6).toArray();
+            res.send(items);
         });
 
-        app.get('/item/:id', async(req, res)=>{
+        app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const item = await itemcollection.findOne(query);
             res.send(item);
         });
 
 
-        app.post('/item', async(req, res) =>{
+        app.post('/item', async (req, res) => {
             console.log(req.body)
-            const newItem =req.body;
+            const newItem = req.body;
             const result = await itemcollection.insertOne(newItem);
             res.send(result);
         });
 
+        app.patch('/update/:id',async(req, res) => {
+            console.log(req.params.id);
+
+           await itemcollection.updateOne({ _id: ObjectId(req.params.id) },
+                {
+                    $set: {quantity:req.body.quantity}
+                })
+                .then(result => {
+
+                    console.log(result);
+                })
+
+        })
 
 
 
 
-        app.delete('/item/:id', async(req, res) =>{
+
+        app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await itemcollection.deleteOne(query);
             res.send(result);
 
@@ -53,7 +67,7 @@ async function run(){
 
     }
 
-    finally{
+    finally {
 
     }
 
@@ -66,12 +80,12 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Running Rideriot server');
 
 
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
